06f5714c52e70c47d954ad4f25316c067c9b280e
a-reznikov <a-reznnikov@gmail.com>
2023-04-08 15:39:46 +0300

    feat: add render cards

--- shelter/pages/main/src/scripts/cards.js
+++ shelter/pages/main/src/scripts/cards.js
@@ -1,5 +1,29 @@
 export class Cards {
-  showText () {
-    console.log("Cards");
-  }
+  constructor({ urlImage, petName }) {
+    this.urlImage = urlImage;
+    this.petName = petName;
+}
+
+generateCard() {
+    let template = '';
+    let cardArticle = document.createElement('article');
+    cardArticle.className = 'layout-3-row card our-friends__card';
+
+    if (this.urlImage) {
+      template += '<div class="card__image">';
+      template += `<img src="${this.urlImage}" alt="pets-${this.petName}" class="card__image-pets">`;
+      template += '</div>';
+    } else {
+      console.log("URl not");
+    }
+
+    if (this.petName) {
+      template += `<h4 class="card__title">${this.petName}</h4>`;
+    }
+
+    template += '<button class="card__button button button_bordered">Learn more</button>';
+
+    cardArticle.innerHTML = template;
+    return cardArticle;
+}
 }
