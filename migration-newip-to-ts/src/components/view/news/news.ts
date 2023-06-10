52e96ab5499d96a21d6804b20bdbab70d7636b59
a-reznikov <a.reznnikov@gmail.com>
2023-06-10 10:50:12 +0300

    fix: correct select html element in file news.ts

--- migration-newip-to-ts/src/components/view/news/news.ts
+++ migration-newip-to-ts/src/components/view/news/news.ts
@@ -7,24 +7,24 @@ class News {
     const fragment: DocumentFragment = document.createDocumentFragment();
     const newsItemTemp: HTMLTemplateElement | null = document.querySelector('#newsItemTemp');
     if (newsItemTemp && newsItemTemp instanceof HTMLElement) {
-      news.forEach((item: ArticlesObject, idx: number) => {
-        const newsClone: Node = newsItemTemp.content.cloneNode(true);
-        const newsItem: HTMLElement | null = (<Element>newsClone.parentNode).querySelector('.news__item');
+      news.forEach((item: ArticlesObject, idx: number): void => {
+        const newsClone: ParentNode = <ParentNode>newsItemTemp.content.cloneNode(true);
+        const newsItem: HTMLElement | null = newsClone.querySelector('.news__item');
         if (idx % 2 && newsItem) newsItem.classList.add('alt');
-        if (newsItem) {
-          const Photo: HTMLElement | null = newsItem.querySelector('.news__meta-photo');
+        if (newsClone) {
+          const Photo: HTMLElement | null = newsClone.querySelector('.news__meta-photo');
           if (Photo) Photo.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
-          const Author: HTMLElement | null = newsItem.querySelector('.news__meta-author');
+          const Author: HTMLElement | null = newsClone.querySelector('.news__meta-author');
           if (Author) Author.textContent = item.author || item.source.name;
-          const Date: HTMLElement | null = newsItem.querySelector('.news__meta-date');
+          const Date: HTMLElement | null = newsClone.querySelector('.news__meta-date');
           if (Date) Date.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
-          const Title: HTMLElement | null = newsItem.querySelector('.news__description-title');
+          const Title: HTMLElement | null = newsClone.querySelector('.news__description-title');
           if (Title) Title.textContent = item.title;
-          const Source: HTMLElement | null = newsItem.querySelector('.news__description-source');
+          const Source: HTMLElement | null = newsClone.querySelector('.news__description-source');
           if (Source) Source.textContent = item.source.name;
-          const description: HTMLElement | null = newsItem.querySelector('.news__description-content');
+          const description: HTMLElement | null = newsClone.querySelector('.news__description-content');
           if (description) description.textContent = item.description;
-          const readMore: HTMLElement | null = newsItem.querySelector('.news__read-more a');
+          const readMore: HTMLElement | null = newsClone.querySelector('.news__read-more a');
           if (readMore) readMore.setAttribute('href', item.url);
         }
         fragment.append(newsClone);
