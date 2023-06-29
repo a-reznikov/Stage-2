b770a7b611c3d46a991afb435f6fc49a34e29d2f
a-reznikov <a.reznnikov@gmail.com>
2023-06-29 12:21:54 +0300

    fix: delete return and change function return type in renderItems.ts

--- rss-css-selectors/src/components/view/renderItems.ts
+++ rss-css-selectors/src/components/view/renderItems.ts
@@ -1,9 +1,8 @@
-export default function renderItems(item: HTMLElement, parent?: HTMLElement): HTMLElement | null {
+export default function renderItems(item: HTMLElement, parent?: HTMLElement): void {
   const tableTop: HTMLElement | null = document.querySelector('.table__top');
   if (parent) {
     parent.append(item);
   } else if (tableTop) {
     tableTop.append(item);
   }
-  return tableTop || null;
 }
