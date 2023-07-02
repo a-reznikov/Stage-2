2bf9b53db87876da71decbcabd592b7f69126948
a-reznikov <a.reznnikov@gmail.com>
2023-07-02 10:18:30 +0300

    feat: add class navigator and implement toggle function to burger menu

---
+++ rss-css-selectors/src/components/controller/navigator.ts
@@ -0,0 +1,24 @@
+class Navigator {
+  public chooseNavButton(event: MouseEvent): void {
+    const hamburger: HTMLInputElement | null = document.querySelector('.hamburger');
+    const previous: HTMLInputElement | null = document.querySelector('.nav__buttons_previous');
+    const next: HTMLInputElement | null = document.querySelector('.nav__buttons_next');
+    const navButton: HTMLElement = <HTMLElement>event.target;
+    if (hamburger && hamburger.contains(navButton)) {
+      this.toggleBurger(hamburger);
+    } else if (previous && previous.contains(navButton)) {
+      console.log(previous);
+    } else if (next && next.contains(navButton)) {
+      console.log(next);
+    }
+  }
+
+  private toggleBurger(hamburgerButton: HTMLElement): void {
+    const asideLevels: HTMLElement | null = document.querySelector('.levels');
+    asideLevels?.classList.toggle('levels__hidden');
+    hamburgerButton?.classList.toggle('hamburger__opened');
+    console.log(hamburgerButton);
+  }
+}
+
+export default Navigator;
--- rss-css-selectors/src/components/model/app.ts
+++ rss-css-selectors/src/components/model/app.ts
@@ -1,8 +1,11 @@
 import Controller from '../controller/controller';
+import Navigator from '../controller/navigator';
 import createTemplate from '../view/template';
 
 class App {
   private controller: Controller = new Controller();
+
+  private navigator: Navigator = new Navigator();
 
   public start(): void {
     createTemplate();
@@ -10,6 +13,7 @@ class App {
     const aside: HTMLElement | null = document.querySelector('.aside');
     const formButton: HTMLElement | null = document.querySelector('.form__button');
     const asideHiddenButton: HTMLElement | null = document.querySelector('.aside__button');
+    const navigation: HTMLElement | null = document.querySelector('.nav');
     if (aside) {
       aside.addEventListener('click', (e: MouseEvent): number => this.controller.getItems(e));
     }
@@ -18,6 +22,9 @@ class App {
     }
     if (asideHiddenButton) {
       asideHiddenButton.addEventListener('click', (e: MouseEvent): void => this.controller.toggleAside(e));
+    }
+    if (navigation) {
+      navigation.addEventListener('click', (e: MouseEvent): void => this.navigator.chooseNavButton(e));
     }
   }
 }
