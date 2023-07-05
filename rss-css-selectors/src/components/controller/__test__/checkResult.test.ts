3d282f285467bfd81f05da22867603dbad8a3293
a-reznikov <a.reznnikov@gmail.com>
2023-07-05 14:27:44 +0300

    feat: add tests to IsLevel function

--- rss-css-selectors/src/components/controller/__test__/checkResult.test.ts
+++ rss-css-selectors/src/components/controller/__test__/checkResult.test.ts
@@ -1,4 +1,4 @@
-import { isAnswer } from '../checkResult';
+import { isAnswer, isLevel } from '../checkResult';
 
 const correctRequests: string[] = ['abcd', '#div', '.span'];
 const incorrectRequests: string[] = ['1abcd', '2#div', '3.span', '100'];
@@ -13,4 +13,35 @@ test('isAnswer function should return false', () => {
   incorrectRequests.forEach((request: string): void => {
     expect(isAnswer(request)).toBeFalsy();
   });
+});
+
+const rightLevelNumber: string = '5';
+const maxLevel: string = '10';
+const overNumber: string = '20';
+const selectorTag: string = 'div';
+const selectorId: string = '#div';
+const selectorClass: string = '.div';
+
+test('isLevel function should return true', () => {
+  expect(isLevel(rightLevelNumber)).toBeTruthy();
+});
+
+test('isLevel function should return true', () => {
+  expect(isLevel(maxLevel)).toBeTruthy();
+});
+
+test('isLevel function should return false', () => {
+  expect(isLevel(overNumber)).toBeFalsy();
+});
+
+test('isLevel function should return false', () => {
+  expect(isLevel(selectorTag)).toBeFalsy();
+});
+
+test('isLevel function should return false', () => {
+  expect(isLevel(selectorId)).toBeFalsy();
+});
+
+test('isLevel function should return false', () => {
+  expect(isLevel(selectorClass)).toBeFalsy();
 });
