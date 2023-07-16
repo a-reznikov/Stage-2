7884a9bb80123516a7b60e7a5bb5a367e8b89e78
a-reznikov <a.reznnikov@gmail.com>
2023-07-16 16:49:17 +0300

    feat: add playground, page and title to garage

--- async-race/src/components/view/create/garage.ts
+++ async-race/src/components/view/create/garage.ts
@@ -4,6 +4,17 @@ export default function createGarage(): HTMLElement {
   const garage: HTMLElement = document.createElement('section');
   garage.className = 'section garage';
   const setup: HTMLElement = createSetup();
+  const title: HTMLElement = document.createElement('h1');
+  title.className = 'garage__title';
+  title.textContent = 'Garage (amount cars)';
+  const page: HTMLElement = document.createElement('p');
+  page.className = 'garage__page';
+  page.textContent = 'Page # 1';
+  const playground: HTMLElement = document.createElement('div');
+  playground.className = 'garage__playground';
   garage.append(setup);
+  garage.append(title);
+  garage.append(page);
+  garage.append(playground);
   return garage;
 }
