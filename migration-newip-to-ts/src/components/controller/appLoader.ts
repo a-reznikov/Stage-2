b76dd044ed23d6d91febda9a7c07884126d5c63c
a-reznikov <a.reznnikov@gmail.com>
2023-10-19 14:21:25 +0300

    feat: change path for dist and api base link

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super(BaseLink.proxy, {
+    super(BaseLink.main, {
       apiKey: ApiKeys.main,
     });
   }
